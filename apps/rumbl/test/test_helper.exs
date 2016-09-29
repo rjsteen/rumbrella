609621b32e5790b4076927f0ad8adf870549a975
Richard Steen <rsteen@covermymeds.com>
2016-09-28 21:26:16 -0500

    tests

--- apps/rumbl/test/test_helper.exs
+++ apps/rumbl/test/test_helper.exs
@@ -1,4 +1,7 @@
+Code.require_file "../../info_sys/test/backends/http_client.exs", __DIR__
 ExUnit.start
 
+Mix.Task.run "ecto.create", ~w(-r Rumbl.Repo --quiet)
+Mix.Task.run "ecto.migrate", ~w(-r Rumbl.Repo --quiet)
 Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, :manual)
 
